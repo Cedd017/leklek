851f62d12bb1a34970960b17dddc92ec63945a2b
Cedd017 <54747333+Cedd017@users.noreply.github.com>
2019-09-01 00:11:43 +0800

    Update cloning.py

--- cloning.py
+++ cloning.py
@@ -60,7 +60,7 @@ class YC:
             "password":self.pw,
             "return_ssl_resources":"0",
             "v":"1.0"}
-        sig = (f'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail={self.email}format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword={self.pw}return_ssl_resources=0v=1.062f8ce9f74b12f84c123cc23437a4a32').encode('utf-8')
+        sig = ('api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail={self.email}format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword={self.pw}return_ssl_resources=0v=1.062f8ce9f74b12f84c123cc23437a4a32').encode('utf-8')
         x = hashlib.new('md5')
         x.update(sig)
         data.update({
